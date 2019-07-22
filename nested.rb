bd1b73899423573d0b78547fdd37a5263adc5c85
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 19:17:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
         {
          :known_for => "Ruby",
          :languages => ["LISP", "C"]
-        }
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]

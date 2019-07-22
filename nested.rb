b184bf0a973e7bcb83294b38831ed9defa04e53a
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 19:14:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz

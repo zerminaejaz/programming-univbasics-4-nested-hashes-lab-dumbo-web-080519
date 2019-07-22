7aa5f5490266d1566f98dceeca70474fca4e646a
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 19:15:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,11 +59,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{

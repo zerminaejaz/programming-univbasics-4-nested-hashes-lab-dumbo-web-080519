e20c73b72fa17d35c92f2758fd997a18e3246e61
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 19:12:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-  puts programmer_hash[:grace_hopper][:known_for]
+   programmer_hash[:grace_hopper][:known_for]
 end
 
 def alan_kay_is_known_for
